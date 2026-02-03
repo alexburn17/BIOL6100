4582f15c12611779e4cd0dfc2d6021e8a3f6d4d6
P. Alexander Burnham <pburnham@uvm.edu>
2026-02-03 12:14:18 -0500

    added files to repository

--- r_scripts/atomicVecs.R
+++ r_scripts/atomicVecs.R
@@ -116,4 +116,129 @@ print(z)
 z <- rep(0, 100)
 length(z)
 
+z <- rep(NA, 100)
+z
 
+typeof(z)
+
+z[1] <- "Vermont"
+head(z)
+typeof(z)
+
+my_vector <- runif(100)
+my_names <- paste("Species",seq(1:length(my_vector)),sep="")
+print(my_names)
+
+names(my_vector) <- my_names
+head(my_vector)
+str(my_vector)
+
+# using the rep function:
+rep(0.5, 6)
+rep(x = 0.5, times = 6)
+
+my_vec <- c(1,2,3)
+
+# repeat a vector
+rep(x=my_vec, times = 2, each = 2)
+
+# sequencing vectors
+seq(from = 2, to = 4)
+2:4 # ok shorthand
+
+x <- seq(from=2, to=4,length=7) # evenly space with a final length of 7
+
+my_vec <- 1:length(x) # common in oth languages
+my_vec # slow!
+
+# better in R
+seq_along(my_vec)
+
+# generating random vectors
+
+runif(5) # gives us 5 values 0 to 1
+
+# the params
+runif(n=3,min=100, max=101)
+
+set.seed(123) # takes any number
+
+# gives you the same progression of random nums
+
+runif(n=1,min=0, max=1)
+
+# normal dists
+out <- rnorm(n = 10, mean = 100, sd = 30)
+hist(out)
+
+# random sampling
+
+long_vec <- seq_len(10)
+
+sample(x=long_vec, size = 10, replace = F)
+
+# weighted sampling from a vec
+weights <- c(rep(10,5),rep(90, 5))
+
+sample(x=long_vec,replace=TRUE,prob=weights)
+
+
+# subsetting vectors
+z <- c(3.1, 9.2, 1.3, 0.4, 7.5)
+print(z)
+
+z[-c(2,3)] # using vecs slice 
+
+# using logicals 
+z[z>3]
+
+z>3
+
+# relational operators
+# <   less than
+# >   greater than
+# <=  less than or equal to
+# >=  greater than or equal to
+# ==  equal to
+
+# logical operators
+# ! not
+# & and (vector)
+# | or (vector)
+# xor(x,y)
+
+
+x <- 1:5
+y <- c(1:3,7,7)
+
+x == 2 # equals
+x != 2 # not equal
+
+x == 5 & y == 7 # using and
+
+x == 1 | y == 7 # using or
+
+x == 3 & y == 3 # and vs or
+
+xor(x==3, y==5)
+
+
+# missing values
+set.seed(90)
+z <- runif(10)
+
+z < 0.5
+z[z < 0.5]
+
+z[which(z < 0.5)]
+
+zd <- c(z, NA, NA)
+
+zd[zd < 0.5]
+
+# dropping NAs with which to index
+zd[which(zd < 0.5)]
+
+which(zd < 0.5)
+
+zd[11]
---
+++ r_scripts/mats_lists_dfs.R
@@ -0,0 +1,134 @@
+# Lists, Matrices, and Data Frames
+# P. Alexander Burnham
+# 29 Jan. 2026
+
+##################################################################
+
+# creating a mat from a vector
+
+my_vec <- 1:12
+
+# numbers by rows
+m <- matrix(data = my_vec, nrow = 4)
+m
+
+# by filling the matrix the other way
+m <- matrix(data = my_vec, ncol = 3, byrow = T)
+m
+
+# lists:
+my_list <- list(1:10, matrix(1:8, nrow = 4, byrow = T), letters[1:3], pi)
+print(my_list)
+
+# indexing a list
+x <- my_list[[1]] # double brackets for object within list element
+str(x)
+
+# indexing into a matrix
+my_list[[2]][1,2] # [rows,cols]
+
+# naming lists
+
+my_list2 <- list(tester=FALSE, little_m = matrix(1:9, nrow = 3))
+my_list2
+
+# named objects in lists:
+my_list2$little_m[2,3]
+
+# looking at empty place indexing
+my_list2$little_m[,1]
+my_list2$little_m[4] # no comma, its a vec
+
+# unlist
+unrolled <- unlist(my_list2)
+unrolled[1]
+
+# unpacking complex lists
+library(ggplot2)
+
+# create some random vars
+y_var <- runif(10)
+x_var <- runif(10)
+
+# regress
+my_model <- lm(y_var~x_var)
+
+# plot it
+qplot(x=x_var, y=y_var) 
+
+# explore structure
+str(summary(my_model))
+
+# extracting p vals
+summary(my_model)$coefficients["x_var","Pr(>|t|)"]
+
+u <- unlist(summary(my_model))
+print(u)
+
+pval <- u$coefficients8
+pval
+
+# data frames
+
+var_a <- 1:12
+var_b <- rep(c("A", "B", "C"), 4)
+var_c <- runif(12)
+
+# creating a data frame from vecs
+df <- data.frame(var_a, var_b, var_c)
+
+str(df)
+
+df$var_a[1]
+
+# expanding the data frame
+new_data <- list(var_a = 13, var_b = "D", var_c = 0.77)
+
+# appending data
+df2 <- rbind(df, new_data)
+
+View(df2) # looking at data frames in the viewer
+
+# add a new column to a df
+df2
+
+# using cbind
+new_var <- rnorm(13)
+df3 <- cbind(df2, new_var)
+
+# using assignment operator
+char_var <- rep("T", 13)
+df3$charV <- char_var
+
+# writing data frames
+write.csv(df3, "data/my_dataframe.csv")
+
+data <- read.csv("data/my_dataframe.csv")
+
+data$var_a
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
